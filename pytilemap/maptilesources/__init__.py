46e5476a8743c76b1714c41d07642e60ea7970dd
Brian Hone <brian.hone@stresearch.com>
2018-01-25 12:49:38 -0500

    pytilemap commit did not go well

--- pytilemap/maptilesources/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function, absolute_import
-
-from .maptilesource import MapTileSource
-from .maptilesourcehere import MapTileSourceHere, MapTileSourceHereDemo
-from .maptilesourceosm import MapTileSourceOSM
-from .maptilesourcehttp import MapTileSourceHTTP
